76bf2db2c8684e1aa8ffded973f3a289678cee8c
Alan <alanyu08@gmail.com>
2025-02-25 20:59:43 -0800

    change to just suroi

--- main.js
+++ main.js
@@ -1,50 +1,33 @@
 // Modules to control application life and create native browser window
-const { app, BrowserWindow, session } = require('electron');
+const { app, BrowserWindow, session, protocol } = require('electron');
 const path = require('node:path');
 
 function createWindow () {
+  protocol.interceptFileProtocol('file', (request, callback) => {
+    const url = request.url.substring(7); // Remove 'file://' prefix
+    const rootPath = path.join(__dirname, 'client'); // Treat 'client/' as root
+    const filePath = path.join(rootPath, url === '/' ? 'index.html' : url);
+    callback({ path: filePath });
+  });
   // Create the browser window.
   const mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
-    autoHideMenuBar: true,
-    webPreferences: {
-      preload: path.join(__dirname, 'preload.js')
-    }
+    fullscreen: true,
+    autoHideMenuBar: true
   })
 
-  mainWindow.loadURL('https://suroi.io');
-
-  // Open the DevTools.
-  //mainWindow.webContents.openDevTools()
+  mainWindow.loadURL(`file:///index.html`);
 }
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
 app.whenReady().then(() => {
   createWindow()
 
-  const filter = {
-    urls: ['https://suroi.io/scripts/main*']
-  }
-  session.defaultSession.webRequest.onBeforeSendHeaders(filter, (details, callback) => {
-    callback({ cancel: true });
-  });
-
   app.on('activate', function () {
-    // On macOS it's common to re-create a window in the app when the
-    // dock icon is clicked and there are no other windows open.
-    if (BrowserWindow.getAllWindows().length === 0) createWindow()
-  })
-})
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
 
-// Quit when all windows are closed, except on macOS. There, it's common
-// for applications and their menu bar to stay active until the user quits
-// explicitly with Cmd + Q.
 app.on('window-all-closed', function () {
-  if (process.platform !== 'darwin') app.quit()
-})
-
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
+  if (process.platform !== 'darwin') app.quit();
+});
--- preload.js
+++ preload.js
@@ -1,6 +1,6 @@
-var _int = setInterval(() => {
+/*var _int = setInterval(() => {
   var script = document.createElement("script");
   script.src = "https://suroihax.glitch.me/script.user.js";
   document.head.prepend(script);
   clearInterval(_int);
-}, 100);
+}, 100);*/
